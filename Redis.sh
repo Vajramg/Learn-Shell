2bc99f27b99c5145b812cc4d3460f1c9030e8b34
Vajram Gajengi <vajramg@gmail.com>
2023-07-11 19:50:47 +0530

    changed print colour

--- Redis.sh
+++ Redis.sh
@@ -1,16 +1,16 @@
-echo -e "\e[31m>>>>>>>>> Install redis repo <<<<<<<<<<\e[om"
+echo -e "\e[33m>>>>>>>>> Install redis repo <<<<<<<<<<\e[om"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-echo -e "\e[31m>>>>>>>>> Module Enable <<<<<<<<<<\e[om"
+echo -e "\e[33m>>>>>>>>> Module Enable <<<<<<<<<<\e[om"
 yum module enable redis:remi-6.2 -y
 
-echo -e "\e[31m>>>>>>>>> Install redis <<<<<<<<<<\e[om"
+echo -e "\e[33m>>>>>>>>> Install redis <<<<<<<<<<\e[om"
 yum install redis -y
 
-echo -e "\e[31m>>>>>>>>> Update redis Listening Address <<<<<<<<<<\e[om"
+echo -e "\e[33m>>>>>>>>> Update redis Listening Address <<<<<<<<<<\e[om"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
 
-echo -e "\e[31m>>>>>>>>> start redis <<<<<<<<<<\e[om"
+echo -e "\e[33m>>>>>>>>> start redis <<<<<<<<<<\e[om"
 systemctl enable redis 
 systemctl start redis
